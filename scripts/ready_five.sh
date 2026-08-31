7f14bcb3da71de6c2cc67d0d31287ccc6f911984
TheSmashy <25392589+TheSmashy@users.noreply.github.com>
2026-08-31 12:18:56 -0500

    Change loop count from 3 to 5 in ready_five.sh

--- scripts/ready_five.sh
+++ scripts/ready_five.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-for i in `seq 1 3`;
-do
-	echo 0 >/sys/class/leds/led0/brightness
-	sleep 0.3
-	echo 1 >/sys/class/leds/led0/brightness
-	sleep 0.3
-done
-
-echo 0 >/sys/class/leds/led0/brightness
-sleep 0.3
+#!/bin/bashfor i in `seq 1 5`;do	echo 0 >/sys/class/leds/led0/brightness	sleep 0.3	echo 1 >/sys/class/leds/led0/brightness	sleep 0.3doneecho 0 >/sys/class/leds/led0/brightnesssleep 0.3
